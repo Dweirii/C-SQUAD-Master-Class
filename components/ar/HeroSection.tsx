1b6312c7e8cc5d72cad55dcb1caaa0c177a028e4
C-SQUAD Zaid <C-SQUAD-Zaid@MacBookPros-MacBook-Pro.local>
2025-06-21 01:51:45 +0300

    improve the video

--- components/ar/HeroSection.tsx
+++ components/ar/HeroSection.tsx
@@ -1,11 +1,6 @@
 "use client"
 
 import Image from "next/image"
-import {
-  Player,
-  Video,
-  DefaultUi,
-} from "@vime/react"
 import "@vime/core/themes/default.css"
 import VideoHero from "../video"
 
